31e852fd42f5033099678a9344612ab78d173af9
faizul-bs23 <faizulcse@gmail.com>
2025-09-18 21:36:52 +0600

    Update event_handler.spec.ts

--- src/tests/e2e/event_handler.spec.ts
+++ src/tests/e2e/event_handler.spec.ts
@@ -5,7 +5,7 @@ test.describe("Event Handler Tests", () => {
     test("Download event test", async ({page}) => {
         await page.goto("https://www.messenger.com/desktop");
 
-        const downloadEvent  = page.waitForEvent("download");
+        const downloadEvent = page.waitForEvent("download");
         await page.getByRole('button', {name: 'Download'}).click();
         const downloadPage = await downloadEvent;
         console.log(`Download URL: ${downloadPage.url()}`);
